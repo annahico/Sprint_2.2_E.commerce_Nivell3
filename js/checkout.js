a565d26b5c02b0d9688e3c7de61b66b0d80a0e92
annahico <annahico@gmail.com>
2025-07-20 12:22:53 +0200

    feat: add additional input fields and error elements for form validation

--- js/checkout.js
+++ js/checkout.js
@@ -4,11 +4,19 @@ const validate = () => {
 	let error = 0;
 	// Get the input fields
 	const fName = document.getElementById("fName");
+	const fLastN = document.getElementById("fLastN");
 	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+  	const fPassword = document.getElementById("fPassword");
+  	const fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	const errorName = document.getElementById("errorName");
+	const errorLastN = document.getElementById("errorLastN");
 	const errorEmail = document.getElementById("errorEmail");  
+	const errorAddress = document.getElementById("errorAddress");
+	const errorPassword = document.getElementById("errorPassword");
+  	const errorPhone = document.getElementById("errorPhone");
 	
 	// Validate fields entered by the user: name, phone, password, and email
 	if(fName.value.trim() == ""){
